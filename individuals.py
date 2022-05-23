9634369e54f88db79452b6a635e74af8da6e48fd
maximeflch <105862262+maximeflch@users.noreply.github.com>
2022-05-23 23:40:31 +0200

    Add files via upload

---
+++ individuals.py
@@ -0,0 +1,24 @@
+
+
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pickle
+
+
+st.set_page_config(
+    page_title= "Heart disease App for individuals",
+    page_icon = "https://icon-library.com/images/heart-disease-icon/heart-disease-icon-7.jpg",
+    layout="wide"
+    )
+
+
+
+
+
+
+
+
+st.write("You want to check out your risk to get a Heart Disease? ")
+st.write("[Click here!](https://www.google.ch/?gfe_rd=cr&dcr=0)")
